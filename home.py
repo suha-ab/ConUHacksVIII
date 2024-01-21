78c33d506a5bd9b04e584982f93a9fb1ee371a76
Inas Fawzi <inasfawzi24@gmail.com>
2024-01-20 22:51:48 -0500

    trying to make start button generate post request

--- home.py
+++ home.py
@@ -1,5 +1,8 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect
 home = Blueprint('home', __name__)
 @home.route('/')
 def landing():
-    return render_template('index.html', boolean=True)
+    if request.method == 'GET':
+        return render_template('index.html', boolean=True)
+    if request.method == 'POST':
+        return redirect()
